bd891b780a3e391f62d21c5dcbb4803062ee01d6
Paulo Vinícius Vieira Toledo <pvvtoledo@gmail.com>
2025-04-25 10:21:44 -0300

    Desgin da página sem tasks finalizado

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,4 +1,5 @@
 import styles from './Tasks.module.css'
+import clipboard from '../assets/Clipboard.svg'
 
 export function Tasks () {
     return (
@@ -13,6 +14,14 @@ export function Tasks () {
                 <div className={styles.done}>
                     <span>Concluídas</span>
                     <span className={styles.counter}>0</span>
+                </div>
+            </div>
+
+            <div className={styles.listContainer}>
+                <img src={clipboard} alt="Imagem de uma prancheta de anotações" />
+                <div className={styles.paragraph}>
+                    <p className={styles.firstLine}>Você ainda não tem tarefas cadastradas</p>
+                    <p className={styles.secondLine}>Crie tarefas e organize seus itens a fazer</p>
                 </div>
             </div>
         </div>
