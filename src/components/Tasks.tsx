9ed394f8ce73a1ccd70ec634d283f4b7148a3d59
Paulo <pvvtoledo@gmail.com>
2025-04-26 10:27:51 -0300

    Adicionada responsividade

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -43,7 +43,7 @@ export function Tasks ({task, onCheckboxChange, completedTasks, handleDeleteTask
                 )
             })
             
-            }
+            } 
             
             
         </div>
